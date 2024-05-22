612e7a5534cbfc38a1d0c8557f03a922e9584b25
Daniel Garcia <templogarcia@comunidad.unam.mx>
2024-05-21 21:56:28 -0600

    Agregando primera vista de bienvenida

--- ReservaYa/mi_app/views.py
+++ ReservaYa/mi_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+# Crear las vistas aqui
 def home(request):
-    return HttpResponse("¡Hola, mundo!")
+    return render(request, 'reservaciones/home.html')
