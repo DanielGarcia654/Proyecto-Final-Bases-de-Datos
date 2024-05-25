a7ce1180a9e99e5efb1cb549640a1bce6b278e91
LeonardoMartinez35 <116773514+leonard3467@users.noreply.github.com>
2024-05-25 03:15:40 -0600

    Update forms.py

--- ReservaYa/mi_app/forms.py
+++ ReservaYa/mi_app/forms.py
@@ -1,7 +1,7 @@
 # forms.py
 
 from django import forms
-from .models import Cliente
+from .models import Cliente, Reservacion
 
 class RegistroForm(forms.ModelForm):
     class Meta:
@@ -11,4 +11,23 @@ class RegistroForm(forms.ModelForm):
             'nombre_cliente': 'Nombre',
             'numero_telefonico': 'Teléfono',
             'correo_electronico': 'Correo Electrónico',
+        }
+class ReservaForm(forms.ModelForm):
+    date = forms.DateField(label='Fecha (YYYY-MM-DD)', widget=forms.DateInput(attrs={'type': 'date'}))
+    time = forms.TimeField(label='Hora (HH:MM  -- p.m/a.m)', widget=forms.TimeInput(attrs={'type': 'time'}))
+    
+    class Meta:
+        model = Reservacion
+        fields = ['id_cliente', 'id_mesa', 'numero_comensales', 'tipo_reserva', 'tiempo_estancia_estimado']
+        labels = {
+            'id_cliente': 'Cliente',
+            'id_mesa': 'Mesa',
+            'numero_comensales': 'Número de Comensales',
+            'tipo_reserva': 'Tipo de Reserva',
+            'tiempo_estancia_estimado': 'Tiempo Estimado de Estancia',
+        }
+        widgets = {
+            'id_cliente': forms.HiddenInput(),  # Puedes cambiar esto según sea necesario
+            'id_mesa': forms.HiddenInput(),  # Puedes cambiar esto según sea necesario
+            'tiempo_estancia_estimado': forms.TextInput(attrs={'placeholder': 'HH:MM:SS'}),
         }
