5c6b875386483d6bd30d89764b507d6558b38046
LeonardoMartinez35 <116773514+leonard3467@users.noreply.github.com>
2024-05-25 03:17:04 -0600

    Update views.py

--- ReservaYa/mi_app/views.py
+++ ReservaYa/mi_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import RegistroForm
+from .forms import RegistroForm, ReservaForm
 from django.http import HttpResponse
+from django.utils import timezone
+from datetime import datetime
+
 
 # Crear las vistas aqui
 def home(request):
@@ -20,3 +23,21 @@ def registro(request):
 
 def registro_exitoso(request):
     return render(request, 'reservaciones/registro_exitoso.html')
+
+def reserva(request):
+    if request.method == 'POST':
+        form = ReservaForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            # Combinar fecha y hora
+            date = form.cleaned_data['date']
+            time = form.cleaned_data['time']
+            fecha_hora_reserva = datetime.combine(date, time)
+            reservation.fecha_hora_reserva = fecha_hora_reserva
+            reservation.save()
+            return redirect('reserva_exitosa')
+        else:
+            return HttpResponse(f"Formulario no valido {form.errors}")
+    else:
+        form = ReservaForm(initial={'id_cliente': request.user.cliente.id})  # Ejemplo de asignación de cliente logueado
+    return render(request, 'reservaciones/reserva.html', {'form': form})
